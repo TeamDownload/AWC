c4157a1826a02edcdb74de9fea030ffc0235653b
Kim minseok <als981209@gmail.com>
2023-05-18 16:54:17 +0900

    Control file

--- screens/Control.tsx
+++ screens/Control.tsx
@@ -4,7 +4,7 @@ import { Text, View ,StyleSheet} from 'react-native'
 const clickCloseButton = () =>{
 
 }
-const clickOpenButton = () =>{
+const clickOpenButton = () =>{ 
     exec().then(()=>{
       console.log("Success");
     });
